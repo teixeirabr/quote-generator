762c160d8c659662a7f3ab88aae6ee3c2bccd87a
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-05-07 16:40:24 -0400

    router set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 import './reset.css'
+import routes from './routes'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<Router>{routes}</Router>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
