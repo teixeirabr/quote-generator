8e1b9210daddebdc833d77b197405a49f0cbb841
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-05-07 16:49:26 -0400

    port bug fixed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
   app = express(),
-  PORT = 4420
+  PORT = process.env.PORT || 4420
 app.use(express.static(`${__dirname}/../build`))
 app.listen(PORT, () => console.log('server running on port:' + PORT))
