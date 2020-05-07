c212e5839b65e313cb147bf31bb5b3803ab7bac2
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-05-07 16:53:12 -0400

    route deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 import './reset.css'
-import routes from './routes'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Router>{routes}</Router>, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import App from './App'
-
-export default (
-  <Switch>
-    <Route exact path='/' component={App} />
-  </Switch>
-)
